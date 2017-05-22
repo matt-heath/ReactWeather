ce010012cf78f78564f1a422cecec4cb5c93c306
Matthew Heath <matthew.heath@hotmail.co.uk>
2017-05-22 15:04:00 +0100

    Style about page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,18 +1,23 @@
 var React = require('react');
 
-// var About = React.createClass({
-//   render: function(){
-//     return(
-//       <h3>About Component</h3>
-//     )
-//   }
-// });
-
 var About = (props) => { //stateless functional component for any component that doesn't maintain a state i.e only returns
     return(
       <div>
-        <h3>About</h3>
-        <p>Welcome to the about page!</p>
+        <h1 className="text-center">About</h1>
+        <p>
+          This is a weather application built using React and OpenWeatherMap. I have built this following the complete React Web app Developer Course.
+        </p>
+        <p>
+          Here are some of the tools I used:
+        </p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react" target="_blank">React</a> - This was the JavaScript framework used.
+          </li>
+          <li>
+            <a href="http://openweathermap.org" target="_blank">OpenWeatherMap</a> - I used the OpenWeatherMap API to search for weather data by city name.
+          </li>
+        </ul>
       </div>
     )
 };
