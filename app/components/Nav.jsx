1be7cc4a28417a5aeb59ff167e7c7f746e3b36c7
Matthew Heath <matthew.heath@hotmail.co.uk>
2017-05-23 12:44:22 +0100

    Remove code for old navigation bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -48,26 +48,3 @@ var Nav = React.createClass({
 });
 
 module.exports = Nav;
-
-var old = (
-  <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <a className="navbar-brand" href="#">Weather</a>
-
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-        </li>
-        <li className="nav-item">
-          <Link to="about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
-)
